8d95fa6d159e76651c40c02de6eabfb88aef047b
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-09-11 10:08:34 +0600

    first commit

---
+++ components/FlatCards.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const FlatCards = () => {
+    return (
+        <View>
+            <Text>
+                Flat Cards
+            </Text>
+        </View>
+    );
+};
+
+
+const style = StyleSheet.create({
+
+})
+
+export default FlatCards;
